1fa28c1cded0dcd6a4b4c844da5621bd16721197
Ranvijay Kumar <ranvijay7021@gmail.com>
2024-12-23 21:23:26 +0530

    Problem Middleware added

--- backend_codebit/controllers/problem.js
+++ backend_codebit/controllers/problem.js
@@ -51,9 +51,8 @@ exports.addProblem=async function(req,res){
 }
 
 
-exports.searchproblem=async function(req,res){
+exports.searchproblem = async function(req,res){
         try {
-
             const {tags,difficulty}=req.body;
 
             if(!tags && !difficulty){
@@ -62,54 +61,71 @@ exports.searchproblem=async function(req,res){
                     message: "Please provide the tag and difficulty"
                 })
             }
+
             if(tags){
 
                 if(!difficulty){
+
                     const problems=await Problem.find({
                         tags:{$all:tags}
                     })
+
                     return res.status(200).json({
                         success:true,
                         message:"All problems with the given tags",
                         problems
                     })
+
                 }
                 else{
+
                     const query={};
                     query.tags=tags;
                     query.difficulty=difficulty;
+
                     const problems=await Problem.find(query);
+
                     return res.status(200).json({
                         success:true,
                         message:"Problems with the given difficulty",
                         problems
                     })
+
                 }
             }
+
             else{
+
                 if(difficulty){
+
                     const problems=await Problem.find({
                         difficulty:{$all:difficulty}
                     })
+
                     return res.status(200).json({
                         success:true,
                         message:"All problems with the given difficulty",
                         problems
                     })
+
                 }
                 else{
+
                     return res.status(400).json({
                         success:false,
                         message: "Please provide the tag or difficulty"
                     })
+
                 }
             }
             
         } catch (error) {
+
             res.status(500).json({
                 success:false,
                 message:"Please Check the tags",
                 error:error.message
             })
+
         }
 }
--- backend_codebit/routes/user.js
+++ backend_codebit/routes/user.js
@@ -1,11 +1,12 @@
 const express=require('express');
 const router=express.Router();
 const {addProblem,searchproblem}=require('../controllers/problem');
+const {checkProblem}=require('../middleware/problem')
 const {login,signup}=require('../controllers/auth'); 
 
 router.post('/login',login);
 router.post('/signup',signup);
-router.post('/problems',addProblem);
+router.post('/problems',checkProblem,addProblem);
 router.get('/problems/search',searchproblem);
 
 
