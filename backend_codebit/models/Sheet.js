f908e89e664e9cbabc7241053235966b8256110a
Ranvijay Kumar <ranvijay7021@gmail.com>
2025-02-12 09:39:29 +0530

    leaderboard added

--- backend_codebit/models/Sheet.js
+++ backend_codebit/models/Sheet.js
@@ -1,41 +1,36 @@
 const mongoose = require('mongoose');
 
+const ProblemSchema = new mongoose.Schema({
+  problem: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Problem',
+    required: true
+  },
+  notes: {
+    type: String,
+    default: ''
+  }
+});
+
+const GroupSchema = new mongoose.Schema({
+  groupName: {
+    type: String,
+    required: true
+  },
+  problems: [ProblemSchema]
+});
+
 const SheetSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
-    required: true
+    required: true,
+    unique: true
   },
-  title: {
-    type: String,
-    required: true
+  groups: {
+    type: [GroupSchema],
+    default: []
   },
-//   description: {
-//     type: String,
-//     default: ''
-//   },
-//   isPublic: {
-//     type: Boolean,
-//     default: false
-//   },
-  groups: [{
-    groupName: {
-      type: String,
-      required: true
-    },
-    problems: [{
-      problem: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Problem',
-        required: true
-      },
-      notes: {
-        type: String,
-        default: ''
-      }
-    }]
-  }],
-  tags: [String],
   createdAt: {
     type: Date,
     default: Date.now
