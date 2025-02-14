9b68d0a2fd09014d793097e7fd3ede947109e373
Ranvijay Kumar <ranvijay7021@gmail.com>
2025-02-15 02:54:37 +0530

    errors resolved

--- backend_codebit/routes/user.js
+++ backend_codebit/routes/user.js
@@ -8,7 +8,7 @@ const {login,signup}=require('../controllers/auth');
 const { addTestCase } = require('../controllers/testCases');
 const {fetchUpcomingContestAPI}=require('../controllers/contest');
 const {executeCode}=require('../controllers/compiler')
-const {Leaderboard,fetchUserDetails}=require('../controllers/leaderBoard');
+const {Leaderboard}=require('../controllers/leaderBoard');
 const { CheckSheet, CreateGroup, DeleteGroup, ShowAllGroups, AddProblemToGroup, RemoveProblemFromGroup } = require('../controllers/userSheet');
 // auth routes
 router.post('/login',login);
@@ -18,11 +18,10 @@ router.post('/problems',checkProblem,addProblem);
 router.get('/problems/search',searchproblem);
 router.post('/problems/addTest',addTestCase);
 // contest routes
-router.post('/contest',fetchUpcomingContest);
+router.post('/contest',fetchUpcomingContestAPI);
 router.post('/execute',executeCode);
 // leaderboard routes
 router.get('/leaderboard',Leaderboard);
-router.get('/leaderboard/leetcode',fetchUserDetails);
 // user sheet routes
 router.get('/sheet/check',CheckSheet);
 router.post('/sheet/group',CreateGroup);
@@ -30,23 +29,5 @@ router.delete('/sheet/group',DeleteGroup);
 router.get('/sheet/groups',ShowAllGroups);
 router.post('/sheet/group/problem',AddProblemToGroup);
 router.delete('/sheet/group/problem',RemoveProblemFromGroup);
-
-// // Check if the sheet exists for the user or validate sheet data
-// router.get('/api/sheets/validate', checkSheet); 
-
-// // Create a new group within a sheet
-// router.post('/api/sheets/:sheetId/groups', createGroup); 
-
-// // Delete a specific group within a sheet
-// router.delete('/api/sheets/:sheetId/groups/:groupId', deleteGroup); 
-
-// // Retrieve all groups from a specific sheet
-// router.get('/api/sheets/:sheetId/groups', showAllGroups); 
-
-// // Add a problem to a specific group in a sheet
-// router.post('/api/sheets/:sheetId/groups/:groupId/problems', addProblemToGroup); 
-
-// // Remove a problem from a specific group in a sheet
-// router.delete('/api/sheets/:sheetId/groups/:groupId/problems/:problemId', removeProblemFromGroup);
 
 module.exports=router;
