000d307a4956f9207cb24e0c1d29c6b50168fa43
sagarAgrawal <sagar.2003kosi@gmail.com>
2025-03-11 14:26:45 +0530

    added _redirects

--- backend_codebit/controllers/auth.js
+++ backend_codebit/controllers/auth.js
@@ -99,7 +99,7 @@ exports.login = async (req, res) => {
       const options = {
         expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production", // ✅ Secure only in production
+        secure: process.env.NODE_ENV === "production", 
         sameSite: "none"
     };
     
